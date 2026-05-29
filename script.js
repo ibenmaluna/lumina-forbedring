62e9edbb0268a180ab256a34878fbcdafead5b76
ibenmaluna <iben2909@gmail.com>
2026-05-29 17:36:56 +0200

    oprettet funktion

--- script.js
+++ script.js
@@ -27,3 +27,26 @@ const produktBillede = document.querySelector(".color-product-img");
 const farveNavn = document.querySelector(".color-info h3");
 const farveKnapper = document.querySelectorAll(".color-cirkle");
 const pileKnapper = document.querySelectorAll(".arrow-btn");
+
+let currentFarve = 0;
+let currentVinkel = 0;
+
+function opdaterProdukt() {
+  const valgtFarve = farver[currentFarve];
+  const valgtVinkel = vinkler[currentVinkel];
+
+    produktBillede.src =
+    `img/products/${valgtFarve.mappe}/${valgtFarve.mappe}-${valgtVinkel}.png`;
+
+  produktBillede.alt =
+    `LUMINA One i farven ${valgtFarve.navn}`;
+
+  farveNavn.textContent = valgtFarve.navn;
+
+  farveKnapper.forEach((knap) => {
+    knap.classList.remove("color-selected");
+  });
+
+  const aktivKnap = document.querySelector(`#${valgtFarve.id}`);
+  aktivKnap.classList.add("color-selected");
+}
