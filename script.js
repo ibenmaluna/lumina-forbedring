8e9b7c90051a2936ef4cbafd0a98014581400a73
ibenmaluna <iben2909@gmail.com>
2026-05-29 17:30:43 +0200

    oprettelse af const farve

---
+++ script.js
@@ -0,0 +1,22 @@
+const farver = [
+  {
+    navn: "Dusty Rose",
+    id: "dusty-rose",
+    billede: "dusty-rose",
+  },
+  {
+    navn: "Moonlight White",
+    id: "moonlight-white",
+    billede: "moonlight-white",
+  },
+  {
+    navn: "Sage Green",
+    id: "sage-green",
+    billede: "sage-green",
+  },
+  {
+    navn: "Lavender Mist",
+    id: "lavender-mist",
+    billede: "lavender-mist",
+  },
+];
